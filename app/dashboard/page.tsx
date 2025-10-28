5817962d7f2c2c95a4880ab0eaac38d115dfd03b
anon-public <allsignin3.0@gmail.com>
2025-10-28 04:49:58 +0000

    Setup of expense cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,21 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "@/components/SearchBar";
 import Addexpense from "@/components/Addexpense";
 
 
 export default function Dashboard({searchParams}: {
     searchParams: { query? : string}
-}) {
+}) {const[query, setquery] = useState('');
+
+    useEffect(() => {
+        setquery(searchParams.query || '');
+    })
     const[istabopen , setistabopen] = useState(false);
     const togglepopup = () => {
         setistabopen(!istabopen);};
     return(
+        <>
        <section className="d-container">           
             <div className="card-stats">
                 <section className="card-text">
@@ -19,12 +24,19 @@ export default function Dashboard({searchParams}: {
                     <p>Total Expenses:</p>
                 </section>
                 </div>     
-                <SearchBar query={searchParams.query || ''} />
+                <SearchBar query={query} />
                     <div className="card-category">SAMPLE</div>  
                 <div>
                     <button className="exp-btn" onClick={togglepopup}>+</button>
                 {istabopen && (<Addexpense isopen={istabopen} onclose={togglepopup}/>)}
                 </div>
             </section>
-);
+
+            <section>
+                    <p className="text-30xl-semibold ">
+                            {query ? `Search results for "${query}` :"All Expenses"}
+                    </p>
+
+            </section>
+</>);
 }
